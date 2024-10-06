c22a618e77cbf9415e723c8d97c58c18049326de
Uditya Narayan Tiwari <uditmerit@gmail.com>
2024-10-06 20:00:38 +0530

    adding largest number in the linear search file

--- src/Basics/Array/_4_Linear_search.java
+++ src/Basics/Array/_4_Linear_search.java
@@ -1,7 +1,25 @@
 package Basics.Array;
+import java.util.*;
 
 public class _4_Linear_search {
-    // creating a function
+
+    // creating a function which is find the largest value in an array
+    public static int getLargest(int[] number){
+        int largest = Integer.MIN_VALUE; // this is for the '-inf' value in the java
+        int smallest = Integer.MAX_VALUE;
+
+        for(int i = 0; i<number.length; i++){
+            if(largest < number[i]){
+                largest = number[i];
+            }
+            if(smallest>number[i]){
+                smallest = number[i];
+            }
+        }
+        System.out.println("Your smallest number present in an array is: "+ smallest);
+        return largest;
+    }
+    // creating a function for search
     public static int Linearsearch(int[] number, int key){
         for(int i = 0; i<number.length; i++){
             if(number[i] == key){
@@ -21,5 +39,7 @@ public class _4_Linear_search {
         else{
             System.out.println("Your key at index: "+ index);
         }
+
+        System.out.println("Largest value present in an array is: "+getLargest(number));
     }
 }
