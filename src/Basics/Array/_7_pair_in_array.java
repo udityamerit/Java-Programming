98ca9c570870aaf91be0a58fda92d933302a791a
Uditya Narayan Tiwari <uditmerit@gmail.com>
2024-10-18 22:22:56 +0530

    adding pairs of an array

---
+++ src/Basics/Array/_7_pair_in_array.java
@@ -0,0 +1,26 @@
+package Basics.Array;
+import java.util.*;
+public class _7_pair_in_array {
+    public static void printpair(int arr[]){
+        for(int i = 0; i<arr.length; i++){
+            int curr = arr[i];
+            for(int j = i+1; j<arr.length; j++){
+                System.out.println("("+arr[i]+", "+ arr[j]+")");
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please Enter the length of the array");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+//        for(int i: arr){
+//            System.out.println(i);
+//        }
+        printpair(arr);
+
+    }
+}
