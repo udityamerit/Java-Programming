9e93207d2a280d235b230684b0df04ca9beb9249
Uditya Narayan Tiwari <uditmerit@gmail.com>
2024-10-20 22:04:59 +0530

    adding the class files

---
+++ src/OppsProgramming/_2_Access_modifiers.java
@@ -0,0 +1,19 @@
+package OppsProgramming;
+import java.util.*;
+
+// creating a class
+class SbiBank{
+    Scanner sc = new Scanner(System.in);
+    public String username;
+    private String password;
+    System.out.println("")
+
+
+
+
+}
+public class _2_Access_modifiers {
+    public static void main(String[] args) {
+
+    }
+}
