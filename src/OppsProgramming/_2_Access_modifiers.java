f33a870ed2659d59680f3ce5da9be4dc7bb979e1
Uditya Narayan Tiwari <uditmerit@gmail.com>
2024-12-22 19:39:21 +0530

    adding the access specifier file

--- src/OppsProgramming/_2_Access_modifiers.java
+++ src/OppsProgramming/_2_Access_modifiers.java
@@ -1,16 +1,37 @@
 package OppsProgramming;
-import java.util.*;
 
-// creating a class
-class SbiBank{
-    Scanner sc = new Scanner(System.in);
-    public String username;
-    private String password;
-//    System.out.println("");
+class Plant{
+    public String name;
+    public final static int ID = 8;
+    private String type;
+    protected String size;
 
+    public Plant(){
+        this.name = "AAM";
+
+        this.type = "oak";
+    }
 }
-public class _2_Access_modifiers {
-    public static void main(String[] args) {
+class oak extends Plant{
+    public oak(){
+//        type = "tree"; // it is not work because the type is private
+        this.size = "largest";
 
     }
+}
+
+public class _2_Access_modifiers {
+    public static void main(String[] args) {
+    Plant p = new Plant();
+    oak o = new oak();
+        System.out.println(p.name);
+        System.out.println(Plant.ID);
+
+        // private value is not accessible
+//        System.out.println(p.type);
+
+        //----this is works because the size is protected variable which is accessible in the subclass ----///
+        System.out.println(o.size);
+    }
+
 }
