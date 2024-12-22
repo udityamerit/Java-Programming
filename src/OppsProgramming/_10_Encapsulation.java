b6418179129e017daa700021dd149b377c14cc8a
Uditya Narayan Tiwari <uditmerit@gmail.com>
2024-12-22 21:14:40 +0530

    adding the encapsulation file

---
+++ src/OppsProgramming/_10_Encapsulation.java
@@ -0,0 +1,26 @@
+package OppsProgramming;
+
+class forest{
+    public static final int ID = 8;
+    private String name;
+
+    public String getData(){
+        String data = "some stuff"+ calculateGrowthForecast();
+        return data;
+    }
+    private int calculateGrowthForecast(){
+        return 9;
+
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(){
+        this.name = name;
+    }
+}
+public class _10_Encapsulation {
+    public static void main(String[] args) {
+
+    }
+}
