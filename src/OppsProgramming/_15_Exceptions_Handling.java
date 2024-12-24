700b7cf2c89d66550542ea03d4573835ee3256f6
Uditya Narayan Tiwari <uditmerit@gmail.com>
2024-12-24 12:46:12 +0530

    adding the exception handling file

---
+++ src/OppsProgramming/_15_Exceptions_Handling.java
@@ -0,0 +1,26 @@
+package OppsProgramming;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.*;
+public class _15_Exceptions_Handling {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("hello.txt");
+        FileReader fr = new FileReader(file);
+
+        // so this is a one way to handle the exception using the throws method
+
+        /* there is another method to resolve the exception handling using hte try and catch block ==>
+
+         instead of writing the throws FileNotFoundException after the main method(public static void main(String[] args) so we are writing the thorws method here we use the :
+
+        try{
+        FileReader fr = new FileReader(file);
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+         */
+
+    }
+}
